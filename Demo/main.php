8be2dc0842b279aabeef2f39207085c89e256c21
Amed Ibanez <amed.ibanez@trueforce.com>
2016-08-26 16:51:12 -0400

    to execute a small demo

---
+++ Demo/main.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Utility\FizzBuzz\FizzBuzzFactory;
+
+$multipliers = [
+    15 => 'FizzBuzz',
+    5 => 'Buzz',
+    3 => 'Fizz',
+];
+
+$factory = new FizzBuzzFactory($multipliers);
+$printer = $factory->createFizzBuzzPrinter(1, 100);
+
+echo $printer->printFizzBuzz();
